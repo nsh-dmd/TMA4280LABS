b1d00ecd467e3964d6a98787693c38555d2cb73d
Neshat <neshat.dmd@gmail.com>
2017-02-20 20:27:16 +0100

    Print output to text file

--- project1/machineFormula/mach0.c
+++ project1/machineFormula/mach0.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-double* gen_vector(double x, size_t n) {
+static const double X1 = 1. / 5;
+static const double X2 = 1. / 239; 
+
+double* gen_machin_vector(double x, size_t n) {
     double *vector = malloc( n * sizeof(double) );
     for (size_t i = 1; i <= n; i++) {
         vector[i] = pow (-1., i-1) * pow(x, 2*i-1) / (2*i-1);
@@ -10,7 +13,7 @@ double* gen_vector(double x, size_t n) {
     return vector;
 }
 
-double reimann_sum(double *vector, size_t n) {
+double sum(double *vector, size_t n) {
     double s = 0;
     for (size_t i = 0; i < n; i++) {
         s += vector[i];
@@ -18,32 +21,60 @@ double reimann_sum(double *vector, size_t n) {
     return s;
 }
 
-double sum_larg(double x) {
-    return arctan(x)
+double machin_formula(int n) {
+  double sum_x1 = sum( gen_machin_vector(X1, n), n );
+  double sum_x2 = sum( gen_machin_vector(X2, n), n );
+  return 16 * sum_x1 - 4 * sum_x2;
 }
 
-double pi_machin() {
-    return 16 * arctan(1./5) - 4 * arctan(1./239);
+double machin_pi() {
+    return 16 * atan(X1) - 4 * atan(X2);
 }
 
-void unit_test(size_t n, ) {
+double abs_error(double s) {
+  return fabs(s - M_PI);
+}
+
+void unit_test(size_t n) {
+
+  double approx = 0.;
+  
+  if ( n < 30 )  approx = machin_formula(n); 
+  else approx = machin_pi();
+
+  printf( "n = %zu \n", n );
+  printf( "Machin pi = %f \n", machin_pi() );
+  printf( "Machin sum = %f \n", approx );
+  printf( "Error = %f \n", abs_error(approx) );
+
+}
+
+void verification_test(char *test_name, int n) {
+
+  double i = 0;
+
+  FILE *pFile = fopen("machin.txt", "w+");
+  fprintf( pFile,"**************************\n %s:\n", test_name );
+  fputs( "**************************\n", pFile );
+
+  if ( n < 12 ) { 
+    for ( size_t k=1; k <= n; k++ ) {
+      i = pow(2., k);
+      fprintf(pFile, "k = %zu \t Error = %f \n", k, abs_error(machin_formula(i)) );
+    }
+  }
+  else fprintf(pFile, "n = %d \t Error = %f \n", n, abs_error(machin_pi()) );
+  
+  fclose(pFile);
 }
 
 int main(int argc, char **argv) {
-    double s;
-    size_t k = atoi(argv[1]);
-    double x = -1 + 2. * (doubel)rand() / RAND_MAX;
     
-    if ( k > 14) sum_larg(x);
-    else {
-        double x = -1 + 2. * (doubel)rand() / RAND_MAX;        
-        double*vector = gen_vector( x, k*k );
-        s = machine_formula(vector, k);
-    }
+    size_t n = atoi(argv[1]);
 
-    printf("reimann_sum = %f \n", s);
-    printf("error = %f \n", fabs(s - pi_machin());
-    
+    unit_test( 3 );
+    verification_test("Machin vtest ", n);
+  
     return 0;
 }
 
--- project1/reimannZeta/zeta0.c
+++ project1/reimannZeta/zeta0.c
@@ -1,66 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 double* gen_reimann_vector(size_t n) {
-    double *vector = malloc( n * sizeof(double) );
-    for (size_t i = 1; i <= n; i++) {
+
+  double *vector = malloc( n * sizeof(double) );
+
+  for (size_t i = 1; i <= n; i++) {
         vector[i] = 1. / (i*i);
-    }
-    return vector;
+  }
+  return vector;
 }
 
-double reimann_sum(double *vector, size_t n) {
-    double sum = 0;
-    for (size_t i = 0; i < n; i++) {
-        sum += vector[i];
-    }
-    return sum;
+double sum(double *vector, size_t n) {
+
+  double s = 0;
+  for (size_t i = 0; i < n; i++)
+    s += vector[i];
+
+  return s;
 }
 
 double reimann_pi() {
-    return (M_PI * M_PI) / 6. ;
+  return (M_PI * M_PI) / 6. ;
 }
 
 double abs_error(double x) {
-  return fabs(x-reimann_pi());
+  return fabs( x - reimann_pi() );
 }
 
 void unit_test(size_t n, double *vector) {
   
-  double sum = 0;
+  printf("unit testing \n");
+  
+  double reimann_approx = 0;
   double pi = reimann_pi();
  
-  if ( n > 30 ) sum = pi;
-  else sum = reimann_sum(vector, n);
+  if ( n > 12 ) reimann_approx = pi;
+  else reimann_approx = sum(vector, n);
    
   printf("n = %zu \n", n );
   printf("reimann pi = %f \n", pi);
-  printf("reimann sum = %f \n", sum);
-  printf("error = %f \n", abs_error(sum));
+  printf("reimann sum = %f \n", reimann_approx);
+  printf("error = %f \n", abs_error(reimann_approx));
+
+  free(vector);
  }
 
 void verification_test(char *test_name, int n) {
 
   double i = 0;
-  double error;
-
-  FILE *file = fopen("vtest.txt", "a+");
-  fprintf(file,"************************************\n %s:\n ************************************\n", test_name);
-  for (size_t k=1; k <= 24; k++) {
-    i = pow(2., k);
-    double *vector = gen_reimann_vector(i) ;
-    fprintf(file, "k = %zu \t Error = %f \n", k, abs_error(reimann_sum(vector, i)) );
+ 
+  FILE *pFile = fopen( "reimann.txt", "w+" );
+  fprintf( pFile,"**************************\n %s n = %d\n", test_name, n );
+  fputs("**************************\\n", pFile );
+  
+  if ( n < 12 ) { 
+    for ( size_t k=1; k <= n; k++ ) {
+      i = pow(2., k);
+      double *vector = gen_reimann_vector(i) ;
+      fprintf(pFile, "k = %zu \t Error = %f \n", k, abs_error(sum(vector, i)) );
+      //free(vector);
+     }
   }
-  fclose(file);
+    
+  else fprintf( pFile, "k = %d \t Error = %f \n", n, abs_error(reimann_pi()) );
+  
+  fclose(pFile);
 }
+
 int main(int argc, char **argv) {
 
   int n = atoi(argv[1]);
 
-  //  double *reimann_vector = gen_reimann_vector( n );
   unit_test( 3, gen_reimann_vector(3) );
-  verification_test("vtest n=2^k", n);
+  verification_test("reimann vtest", n);
 
   return 0;
 }
