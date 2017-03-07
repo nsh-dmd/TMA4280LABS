5bda3b92ba093ac41063d5e6e8bd35512ea15b20
Neshat <neshat.dmd@gmail.com>
2017-03-07 15:24:22 +0100

    vtest for both zeta and mach in one file

--- project1/vtest.c
+++ project1/vtest.c
@@ -1,32 +1,85 @@
-#include "util.h"
-#include "zeta0.c"
-#include "mach0.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define PI 3.14159265358979323846
+#define X1 1. / 5
+#define X2 1. / 239
+
+double sum (double *vector, size_t n) {
+  double s = 0;
+  for (size_t i = 0; i < n; i++)
+    s += vector[i];
+  return s;
+}
+
+double abs_error(double s) {
+  return fabs(s - PI);
+}
+double* gen_reimann_vector (size_t n) {
+  double *vector = (double*) malloc( n * sizeof(double) );
+   for (size_t i = 1; i <= n; i++) {
+        vector[i] = 1. / (i*i);
+  }
+  return vector;
+}
+
+double reimann_pi (size_t n) {
+    double *v = gen_reimann_vector(n);
+    double pi = sqrt(6.0 * sum(v , n));
+    free(v);
+    return pi;
+}
+double* gen_machin_vector(double x, size_t n) {
+    double *vector = (double*) malloc( n * sizeof(double) );
+    for (size_t i = 1; i <= n; i++) {
+        vector[i] = pow (-1., i-1) * pow(x, 2*i-1) / (2*i-1);
+    }
+    return vector;
+}
+
+double machin_formula(int n) {
+    double *v1 = gen_machin_vector(X1, n);
+    double *v2 = gen_machin_vector(X2, n);
+    double pi = 16 * sum(v1, n) - 4 * sum( v2, n );
+    free(v1);
+    free(v2);
+    return pi;
+}
+
+double machin_pi() {
+    return 16 * atan(X1) - 4 * atan(X2);
+}
 
 void verification_test(char *test_name, int n) {
 
-  double i, error = 0;
+  double i, error_m = 0, error_z = 0;
 
   FILE *pFile = fopen("vtest.txt", "w+");
-  fprintf( pFile,"**************************\n %s:\n", test_name );
+  fprintf( pFile,"**************************\n Verification test:\n" );
   fputs( "**************************\n", pFile );
 
   if ( n < 25 ) {
     for ( size_t k=1; k <= n; k++ ) {
       i = pow(2., k);
-      switch (test_name) {
-          case 'machin': error = abs_error(machin_formula(i)); break;
-          case 'reimann': error = abs_error(reimann_pi(i)); break;
-      }
-      fprintf(pFile, "k = %zu \t Error = %f \n", k,  error);
+      error_m = abs_error(machin_formula(i));
+      error_z = abs_error(reimann_pi(i));
+      fprintf(pFile, "k = %zu \t Mach error = %.10e \n", k,  error_m);
+      fprintf(pFile, "k = %zu \t Zeta error = %.10e \n", k,  error_z);
+      fputs( "**************************\n", pFile );
     }
   }
-  else fprintf(pFile, "n = %d \t Error = %f \n", n, abs_error(machin_pi()) );
+  // else fprintf(pFile, "n = %d \t Error = %f \n", n, abs_error(machin_pi()) );
+  else printf("ERROR! n must be less than 25!\n");
 
   fclose(pFile);
 }
 
 int main(int argc, char const **argv) {
     int n = atoi(argv[1]);
-    verification_test(n);
+    verification_test("machin", n);
+    verification_test("reimann", n);
+
     return 0;
 }
